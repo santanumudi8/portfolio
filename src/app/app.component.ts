90c4b58fb90fd045e2d146a4fa003b1e00c94a23
Santanu Mudi <santanumudi8@gmail.com>
2023-12-25 23:44:09 +0530

    modified the design

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, ViewChild } from '@angular/core';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +6,9 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'music-player';
+  title = 'protfolio';
+  scroll(el:HTMLElement){
+    el.scrollIntoView({behavior: 'smooth'});
+  }
+
 }
